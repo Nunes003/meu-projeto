f413292ae221d1db2135fb1f6f0c9f0a8cb9807c
Nunes <j.m.a.nunes06@gmail.com>
2025-03-06 11:00:52 +0000

    text

--- src/Components/info-text/about_me.js
+++ src/Components/info-text/about_me.js
@@ -2,14 +2,16 @@ export default function AboutMe() {
   return (
     <div className="my-text">
       <div className="about-me">
-        <img className="about-me-icon" src="./student-svgrepo-com.svg" alt="eu" />
+        <img
+          className="about-me-icon"
+          src="./student-svgrepo-com.svg"
+          alt="eu"
+        />
         <span className="about-me-text">Acerca de Mim</span>
       </div>
 
       <div className="about-me-description">
-        <span>
-          text
-        </span>
+        <span>text</span>
       </div>
     </div>
   );
