28265cb0135508b52e465cdd66a9c5961bbcfc43
Nunes <j.m.a.nunes06@gmail.com>
2025-03-06 11:00:16 +0000

    text me

--- src/Components/info-text/about_me.js
+++ src/Components/info-text/about_me.js
@@ -8,18 +8,7 @@ export default function AboutMe() {
 
       <div className="about-me-description">
         <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          tortor quam, fermentum id semper nec, feugiat sit amet metus. Nullam
-          suscipit neque sem, eget accumsan odio aliquet sed. Aliquam sit amet
-          faucibus nunc. Quisque dignissim nunc in tellus finibus porttitor.
-          Nunc lobortis mauris aliquet quam mattis, quis semper ipsum molestie.
-          Nam lectus dolor, iaculis sed tincidunt iaculis, faucibus non sapien.
-          Maecenas lacinia elementum pharetra. Quisque sed tellus turpis. Nunc
-          luctus justo ac ante tristique cursus. Integer lorem metus, mattis
-          vitae euismod at, sollicitudin eu ex. Donec sagittis lacus sed est
-          fringilla, non vehicula quam vehicula. Phasellus luctus tempor nibh,
-          quis tincidunt eros tristique sit amet. Cras ut ipsum ornare, posuere
-          risus id, sollicitudin nisi.
+          text
         </span>
       </div>
     </div>
